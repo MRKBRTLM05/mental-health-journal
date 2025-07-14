825adbcd9a87dc511a7e954967eaaab653a589bc
MRKBRTLM05 <bartolomemarkjosephr@gmail.com>
2025-07-14 17:32:49 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/mental-health-journal/', // <-- folder name in your GitHub repo
+  base: "/mental-health-journal/", // <-- folder name in your GitHub repo
   plugins: [react()],
-})
+});
